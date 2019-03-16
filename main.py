e5958714cfdbf9e2207439410d8643456cff6e09
Derek Li <derekgolf72@yahoo.com>
2019-03-16 01:35:06 -0500

    Added basic parsing for new line, tabs, and \r characters

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 #All one liners here
 
 
+javaString = '''public class HelloWorld {
 
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}'''
 
+def convertToOneLiner(javaString):
+	javaString = javaString.strip()
 
+	charactersToReplace = ["\n", "\t", "\r"]
+	formattedString = ""
+	for line in javaString.splitlines():
+		line = line.strip()
+		for character in charactersToReplace:
+			line = line.replace(character, "")
+		formattedString += line
+			
+	return formattedString
+
+print(convertToOneLiner(javaString))
